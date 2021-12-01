f18d56554cd44f7dc0ab7d0f1017cf38f0236607
anttu2 <andreas.s.backman@gmail.com>
2021-12-01 22:25:34 +0200

    Added solution for 2021 and Day1

---
+++ 2021/AoC2021/AoC2021.Tests/FileHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace AoC2021.Tests;
+
+public class FileHelper
+{
+    public static async Task<string[]> GetInputAsLines(string day)
+    {
+        return await File.ReadAllLinesAsync($"InputData/{day}.txt");
+    }
+
+    public static async Task<T[]> GetInputAs<T>(string day)
+    {
+        var input = await GetInputAsLines(day);
+        return input
+            .Select(i => (T)Convert.ChangeType(i, typeof(T)))
+            .ToArray();
+    }
+
+    public static async Task<string> GetInputAsText(string day)
+    {
+        return await File.ReadAllTextAsync($"InputData/{day}.txt");
+    }
+}
