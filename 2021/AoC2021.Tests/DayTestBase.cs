3c93ce60de1cca24ef2e93b4fee82f3f44d2eac5
Andreas Backman <andreas.backman@gambitgroup.fi>
2021-12-02 18:11:07 +0200

    Fixed folders

---
+++ 2021/AoC2021.Tests/DayTestBase.cs
@@ -0,0 +1,12 @@
+﻿namespace AoC2021.Tests;
+
+public abstract class DayTestBase
+{
+    protected ITestOutputHelper Output;
+    protected string Day => this.GetType().Name.Substring(0, 4);
+
+    public DayTestBase(ITestOutputHelper output)
+    {
+        Output = output;
+    }
+}
---
+++ 2021/AoC2021/Day1.cs
@@ -0,0 +1,19 @@
+﻿namespace AoC2021;
+public class Day1
+{
+    public int FindIncreaseCount(int[] testInput)
+    {
+        return Enumerable.Range(0, testInput.Length - 1)
+            .Select(i => testInput[i+1] > testInput[i] ? 1 : 0)
+            .Sum();
+    }
+
+    public int FindIncreaseCountWithSlidingWindow(int[] testInput)
+    {
+        var windows = Enumerable.Range(0, testInput.Length - 2)
+            .Select(i => testInput[i] + testInput[i + 1] + testInput[i + 2])
+            .ToArray();
+
+        return FindIncreaseCount(windows);
+    }
+}
