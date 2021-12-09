dc720bd18eb028f4a01eede5d19e492a3f02aa8a
Andreas Backman <andreas.backman@gambitgroup.fi>
2021-12-09 22:46:52 +0200

    Cleaned up day 6

--- 2021/AoC2021.Tests/Day6Tests.cs
+++ 2021/AoC2021.Tests/Day6Tests.cs
@@ -1,12 +1,9 @@
 namespace AoC2021.Tests;
 public class Day6Tests : DayTestBase
 {
-    private readonly Day6 _day6;
     private readonly string _testInput = "3,4,3,1,2" ;
 
-    public Day6Tests(ITestOutputHelper output) : base(output)
-    {
-    }
+    public Day6Tests(ITestOutputHelper output) : base(output) { }
 
     [Theory]
     [InlineData(18, 26)]
@@ -31,36 +28,22 @@ public class Day6Tests : DayTestBase
     
     
     [Theory]
-    [InlineData(18, 26)]
-    [InlineData(80, 5934)]
     [InlineData(256,26984457539)]
     public void Part2(int daysToRun, long expectedFishCount)
     {
-        var day6 = new Day6.Optimized(_testInput);
+        var day6 = new Day6(_testInput);
         var result = day6.RunSimulationAndCountFishes(daysToRun);
         
         Assert.Equal(expectedFishCount, result);
     }
     
-    [Theory]
-    [InlineData(18, 26)]
-    [InlineData(80, 5934)]
-    [InlineData(256,26984457539)]
-    public void Part2Optimized(int daysToRun, long expectedFishCount)
+    [Fact]
+    public async Task Part2RealData()
     {
-        var day6 = new Day6.Optimized2(_testInput);
-        var result = day6.RunSimulationAndCountFishes(daysToRun);
-        
-        Assert.Equal(expectedFishCount, result);
+        var realInput = await FileHelper.GetInputAsText(Day);
+        var day6 = new Day6(realInput);
+        var result = day6.RunSimulationAndCountFishes(256);
+    
+        Output.WriteLine(result.ToString());
     }
-    //
-    // [Fact]
-    // public async Task Part2RealData()
-    // {
-    //     var realInput = await FileHelper.GetInputAsText(Day);
-    //     var day6 = new Day6(realInput);
-    //     var result = day6.CountOverlappingPoints();
-    //
-    //     Output.WriteLine(result.ToString());
-    // }
 }
--- 2021/AoC2021/Day6.cs
+++ 2021/AoC2021/Day6.cs
@@ -4,162 +4,40 @@ namespace AoC2021;
 
 public class Day6 : IDay
 {
-    private Optimized _optimized;
-    private Optimized2 _optimized2;
-
-    class LanternFish
-    {
-        public int DaysUntilSpawn { get; private set; } = 8;
-
-        public LanternFish()
-        {
-        }
-        
-        public LanternFish(int daysUntilSpawn)
-        {
-            DaysUntilSpawn = daysUntilSpawn;
-        }
-
-        public LanternFish? DayPassed()
-        {
-            DaysUntilSpawn--;
-
-            LanternFish newFish = null;
-
-            if (DaysUntilSpawn < 0)
-            {
-                DaysUntilSpawn = 6;
-                newFish = new LanternFish();
-            }
-
-            return newFish;
-        }
-    }
-
-    private List<LanternFish> _fishes = new();
+    private long[] _fishCounts = new long[9];
 
     public Day6(string input)
     {
         ParseFishes(input);
-        _optimized = new Optimized(input);
-        _optimized2 = new Optimized2(input);
     }
 
-    private void ParseFishes(string testInput)
+    private void ParseFishes(string fishData)
     {
-        var fishDays = testInput.Split(",");
+        var fishDays = fishData.Split(",");
 
         foreach (var fishDay in fishDays)
         {
-            _fishes.Add(new LanternFish(int.Parse(fishDay)));
+            var daysOld = int.Parse(fishDay);
+            _fishCounts[daysOld]++;
         }
     }
 
-    public int RunSimulationAndCountFishes(int daysToRun)
+    public long RunSimulationAndCountFishes(int daysToRun)
     {
-        for (int i = 0; i < daysToRun; i++)
+        for (int day = 0; day < daysToRun; day++)
         {
-            var newFishesToAdd = new List<LanternFish>();
-            foreach (var fish in _fishes.AsParallel())
+            var newbies = _fishCounts[0];
+
+            for (int i = 1; i <= 8; i++)
             {
-                var newFish = fish.DayPassed();
-
-                if (newFish != null)
-                {
-                    newFishesToAdd.Add(newFish);
-                }
-            }
-            
-            _fishes.AddRange(newFishesToAdd);
-        }
-
-        return _fishes.Count();
-    }
-    
-    public class Optimized
-    {
-        private readonly List<List<byte>> _fishBuckets = new();
-
-        public Optimized(string fishData)
-        {
-            ParseFishes(fishData);
-        }
-        
-        private void ParseFishes(string fishData)
-        {
-            var fishDays = fishData.Split(",");
-        
-            foreach (var fishDay in fishDays)
-            {
-                _fishBuckets.Add(new List<byte> {byte.Parse(fishDay)});
-            }
-        }
-        
-        public long RunSimulationAndCountFishes(int daysToRun)
-        {
-            for (int i = 0; i < daysToRun; i++)
-            {
-                Console.WriteLine($"Day: {i}");
-                
-                Parallel.ForEach(_fishBuckets, fishBucket =>
-                    // foreach (var fishBucket in _fishBuckets)
-                {
-                    var bucketSize = fishBucket.Count;
-                    for (int j = 0; j < bucketSize; j++)
-                    {
-                        var fishDay = --fishBucket[j];
-
-                        if (fishDay == byte.MaxValue)
-                        {
-                            fishBucket[j] = 6;
-                            fishBucket.Add(8);
-                        }
-                    }
-                //}
-                });
-            }
-        
-            return _fishBuckets.Select(fb => fb.LongCount()).Sum();
-        }
-    }
-
-    public class Optimized2
-    {
-        private long[] _fishCounts = new long[9];
-
-        public Optimized2(string input)
-        {   
-            ParseFishes(input);
-        }
-        
-        private void ParseFishes(string fishData)
-        {
-            var fishDays = fishData.Split(",");
-        
-            foreach (var fishDay in fishDays)
-            {
-                var daysOld = int.Parse(fishDay);
-                _fishCounts[daysOld]++;
-            }
-        }
-
-        public long RunSimulationAndCountFishes(int daysToRun)
-        {
-            for (int day = 0; day < daysToRun; day++)
-            {
-                var newbies = _fishCounts[0];
-                
-                for (int i = 1; i <= 8; i++)
-                {
-                    _fishCounts[i - 1] = _fishCounts[i];
-                }
-
-                _fishCounts[6] += newbies;
-                _fishCounts[8] = newbies;
+                _fishCounts[i - 1] = _fishCounts[i];
             }
 
-            return _fishCounts.Sum();
+            _fishCounts[6] += newbies;
+            _fishCounts[8] = newbies;
         }
+
+        return _fishCounts.Sum();
     }
 
     public Task<string> Part1(string input, params string[] args)
@@ -170,7 +48,6 @@ public class Day6 : IDay
 
     public Task<string> Part2(string input, params string[] args)
     {
-        var count = _optimized2.RunSimulationAndCountFishes(int.Parse(args.Last()));
-        return Task.FromResult(count.ToString());
+        return Part1(input, args);
     }
 }
